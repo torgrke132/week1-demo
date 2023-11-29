4cc5485eb02d9c3133c0a2352707d91324d30005
ju-voro <voronetskajaj@hotmail.com>
2023-11-29 13:19:03 +0100

    add files

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log('hello from index.js');
+
+// let firstName = "Harry Potter";
+// let age = 11;
+// let isWizard = true;
+
+// console.log(typeof(firstName));
+// console.log(typeof(age));
+// console.log(typeof(isWizard));
+
+//explain differences between const and let
+
+const firstName = prompt('Please, enter your first name.', 'enter your name');
+console.log(`hello ${firstName}`);
+
+const age = prompt(`How old are you, ${firstName}?`);
+console.log(age);
+
+const areYouAWizard = prompt('Are you a wizard? Y/N');
+console.log(areYouAWizard);
+
+if(areYouAWizard == 'Y') {
+    
+    
+}
